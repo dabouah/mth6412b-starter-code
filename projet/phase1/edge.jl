23b9353f26a49c968fbf4da92f08747b706fc339
dabouah <daphne.boulanger@polymtl.ca>
2023-11-03 13:57:35 -0400

    ajout de l'attribut name

--- projet/phase1/edge.jl
+++ projet/phase1/edge.jl
@@ -17,6 +17,12 @@ mutable struct Edge{W,T} <: AbstractEdge{W,T}
   node_1::Node{T}
   node_2::Node{T}
   weight::W
+  name::String
+end
+
+"""Crée une arete sans nom"""
+function Edge(node_1::Node{T}, node_2::Node{T}, weight::W) where {W,T}
+  Edge(node_1::Node{T}, node_2::Node{T}, weight::W, name(node_1)*name(node_2))
 end
 
 # on présume que tous les arêtes dérivant d'AbstractEdge
@@ -27,6 +33,9 @@ nodes(edge::AbstractEdge) = [edge.node_1, edge.node_2]
 
 """Renvoie le poids de l'arête."""
 weight(edge::AbstractEdge) = edge.weight
+
+"""Renvoie le nom de l'arête."""
+name(edge::AbstractEdge) = edge.name
 
 """Crée une arête avec un poids par défaut"""
 function Edge(node_1::Node{T}, node_2::Node{T}) where T
